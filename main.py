e5821fcccb9c858594df51570fd67e49cf08b52d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-28 05:00:05 -0400

    Add top 10k data fetching

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from datetime import datetime
 
-from src.fetch.fpl import fetch_bootstrap_static
+from src.fetch.fpl import fetch_bootstrap_static, fetch_event_status
 from src.update.clubelo import update_clubelo
 from src.update.fpl import update_fpl
 from src.update.understat import update_understat
@@ -22,14 +22,28 @@ def main():
 def update():
     """Fetch and update data from all sources."""
 
-    # Determine the current season and the next gameweek
+    # Fetch current data and game state
     bootstrap_static = fetch_bootstrap_static()
+    event_status = fetch_event_status()
     static_events = bootstrap_static["events"]
     current_season = get_current_season(static_events)
+    current_gameweek = get_current_gameweek(static_events)
     next_gameweek = get_next_gameweek(static_events)
 
+    if current_season == "2025-26":
+        overall_league_id = 314
+    else:
+        raise ValueError("Overall league ID for current season is not set.")
+
     # Update all data
-    update_fpl(current_season, next_gameweek, bootstrap_static)
+    update_fpl(
+        current_season,
+        current_gameweek,
+        next_gameweek,
+        overall_league_id,
+        bootstrap_static,
+        event_status,
+    )
     update_clubelo(bootstrap_static)
     update_understat(current_season, bootstrap_static)
 
@@ -37,6 +51,13 @@ def update():
 def get_current_season(static_events: list[dict]) -> str:
     year = datetime.fromisoformat(static_events[0]["deadline_time"]).year
     return f"{year}-{str(year + 1)[-2:]}"
+
+
+def get_current_gameweek(static_events: list[dict]) -> int | None:
+    for event in static_events:
+        if event["is_current"]:
+            return event["id"]
+    return None
 
 
 def get_next_gameweek(static_events: list[dict]) -> int | None:
--- src/update/fpl.py
+++ src/update/fpl.py
@@ -1,12 +1,27 @@
 from tqdm import tqdm
 
-from src.fetch.fpl import fetch_element_summary, fetch_fixtures
+from src.fetch.fpl import (
+    fetch_element_summary,
+    fetch_entry_picks,
+    fetch_fixtures,
+    fetch_league_standings,
+)
 from src.utils import DATA_DIR, write_csv, write_json_xz
 
 
-def update_fpl(current_season: str, next_gameweek: int | None, bootstrap_static: dict):
+def update_fpl(
+    current_season: str,
+    current_gameweek: int | None,
+    next_gameweek: int | None,
+    overall_league_id: int,
+    bootstrap_static: dict,
+    event_status: dict,
+):
     # Update bootstrap static
-    path = DATA_DIR / f"fpl/{current_season}/static/{next_gameweek}.json.xz"
+    if next_gameweek:
+        path = DATA_DIR / f"fpl/{current_season}/static/{next_gameweek}.json.xz"
+    else:
+        path = DATA_DIR / f"fpl/{current_season}/static/final.json.xz"
     write_json_xz(bootstrap_static, path)
 
     # Update fixtures
@@ -22,3 +37,22 @@ def update_fpl(current_season: str, next_gameweek: int | None, bootstrap_static:
         history = element_summary["history"]
         path = DATA_DIR / f"fpl/{current_season}/elements/{element_id}.csv"
         write_csv(history, path)
+
+    # Update top 10K picks. This should be done only once for each gameweek.
+    if (current_gameweek is not None) and (event_status["leagues"] == "Updated"):
+        path = DATA_DIR / f"fpl/{current_season}/top_10k/{current_gameweek}.json.xz"
+        if not path.exists():
+            standings = []
+            for i in tqdm(range(1, 201), desc="Fetching top 10k standings"):
+                data = fetch_league_standings(overall_league_id, i)
+                standings.extend(data["standings"]["results"])
+
+            picks = {}
+            for item in tqdm(standings, desc="Fetching top 10k picks"):
+                data = fetch_entry_picks(item["entry"], current_gameweek)
+                picks[item["entry"]] = data
+
+            for manager in standings:
+                manager["picks"] = picks[manager["entry"]]
+
+            write_json_xz(standings, path)
