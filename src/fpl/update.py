cb8ea08fd0a2a253ac964f76bc9011de5642fd4d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-28 20:06:08 -0400

    Refactor FPL update functions

--- src/fpl/update.py
+++ src/fpl/update.py
@@ -17,19 +17,29 @@ def update_fpl(
     bootstrap_static: dict,
     event_status: dict,
 ):
-    # Update bootstrap static
+    update_bootstrap_static(current_season, next_gameweek, bootstrap_static)
+    update_fixtures(current_season)
+    update_elements(current_season, bootstrap_static)
+    update_top_10k(current_season, current_gameweek, overall_league_id, event_status)
+
+
+def update_bootstrap_static(
+    current_season: str, next_gameweek: int | None, bootstrap_static: dict
+):
     if next_gameweek:
         path = DATA_DIR / f"fpl/{current_season}/static/{next_gameweek}.json.xz"
     else:
         path = DATA_DIR / f"fpl/{current_season}/static/final.json.xz"
     write_compressed_json(bootstrap_static, path)
 
-    # Update fixtures
+
+def update_fixtures(current_season: str):
     fixtures = fetch_fixtures()
     path = DATA_DIR / f"fpl/{current_season}/fixtures.csv"
     write_csv(fixtures, path)
 
-    # Update elements
+
+def update_elements(current_season: str, bootstrap_static: dict):
     static_elements = bootstrap_static["elements"]
     element_ids = [element["id"] for element in static_elements]
     for element_id in tqdm(element_ids, desc="Updating elements"):
@@ -38,21 +48,31 @@ def update_fpl(
         path = DATA_DIR / f"fpl/{current_season}/elements/{element_id}.csv"
         write_csv(history, path)
 
-    # Update top 10K picks. This should be done only once for each gameweek.
-    if (current_gameweek is not None) and (event_status["leagues"] == "Updated"):
-        path = DATA_DIR / f"fpl/{current_season}/top_10k/{current_gameweek}.json.xz"
-        if not path.exists():
-            standings = []
-            for i in tqdm(range(1, 201), desc="Fetching top 10k standings"):
-                data = fetch_league_standings(overall_league_id, i)
-                standings.extend(data["standings"]["results"])
 
-            picks = {}
-            for item in tqdm(standings, desc="Fetching top 10k picks"):
-                data = fetch_entry_picks(item["entry"], current_gameweek)
-                picks[item["entry"]] = data
+def update_top_10k(
+    current_season: str,
+    current_gameweek: int | None,
+    overall_league_id: int,
+    event_status: dict,
+):
+    # Retrieve standings only once and after each gameweek
+    if (current_gameweek is None) or (event_status["leagues"] != "Updated"):
+        return
+    path = DATA_DIR / f"fpl/{current_season}/top_10k/{current_gameweek}.json.xz"
+    if path.exists():
+        return
 
-            for manager in standings:
-                manager["picks"] = picks[manager["entry"]]
+    standings = []
+    for i in tqdm(range(1, 201), desc="Fetching top 10k standings"):
+        data = fetch_league_standings(overall_league_id, i)
+        standings.extend(data["standings"]["results"])
 
-            write_compressed_json(standings, path)
+    picks = {}
+    for item in tqdm(standings, desc="Fetching top 10k picks"):
+        data = fetch_entry_picks(item["entry"], current_gameweek)
+        picks[item["entry"]] = data
+
+    for manager in standings:
+        manager["picks"] = picks[manager["entry"]]
+
+    write_compressed_json(standings, path)
